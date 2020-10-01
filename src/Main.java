e7d27286e8a15737ec434bc4dd97ccd2fa103926
emily-sear <61364884+emily-sear@users.noreply.github.com>
2020-10-01 11:53:44 -0500

    Fixed the ArrayList to add <String>

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,18 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class Main
 {
 	public static void main(String[] args) throws FileNotFoundException
 	{
-		ArrayList dataList = new ArrayList();
+		ArrayList<String> dataList = new ArrayList<String>();
 		Scanner input = new Scanner(new File(System.getProperty("user.dir") + "/src/data.dat"));
 		while(input.hasNext())
 		{
 			dataList.add(input.nextLine());
 		}
+		
 		/**
 		
 		for(int i = 0; i < dataList.size(); i++)
@@ -20,10 +22,11 @@ public class Main
 			System.out.println(dataList.get(i).toString());
 		}
 		**/
+		
 		BibleBook[] newTestament = new BibleBook[dataList.size()];
 		for(int i = 0; i < newTestament.length; i++)
 		{
-			newTestament[i] = new BibleBook(dataList.get(i).toString());
+			newTestament[i] = new BibleBook(dataList.get(i));
 			newTestament[i].displayBook();
 			System.out.println();
 		}
